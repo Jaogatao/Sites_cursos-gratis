e3f54be912a520353c861078e960635e0bed47ce
João Antônio <105800868+Jaogatao@users.noreply.github.com>
2023-06-12 00:20:03 -0300

    Site Atualizado

---
+++ Site_Cursos_Gratis-main/oxe/index.js
@@ -0,0 +1,19 @@
+function cursoselecionado()
+{
+    window.location.href ="index.html";
+   
+}
+
+function home()
+{
+    window.location.href= "home2.html";
+}
+
+function favoritos(){
+ window.location.href= "cursoselecionado.html"
+}
+
+function login(){
+
+    window.location.href= "Login.html"
+}
